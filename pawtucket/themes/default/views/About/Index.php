85ef05625a947d032c559e0fd7272c53be2ce548
Adeline Gueniat <gueniat.adeline@gmail.com>
2019-10-28 11:02:14 +0100

    add short description of Pro Ethnographica to the about page

--- pawtucket/themes/default/views/About/Index.php
+++ pawtucket/themes/default/views/About/Index.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 	MetaTagManager::setWindowTitle($this->request->config->get("app_display_name").": About");
 ?>
 
@@ -9,8 +9,35 @@
 	</div>
 	<div class="row">
 		<div class="col-sm-8">
-			<h3>Contact The Archives</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec ligula erat. Pellentesque nibh leo, pharetra et posuere vel, accumsan vitae sapien. Phasellus a tortor id felis scelerisque blandit. Curabitur a tristique tortor. Morbi non tortor eget dui blandit laoreet. Quisque lacus quam, auctor sit amet volutpat dictum, scelerisque sit amet neque. Vivamus non massa finibus, ultrices nunc vel, scelerisque dui. Aliquam commodo, quam eget fringilla finibus, enim diam sodales ligula, sollicitudin faucibus ligula lorem vitae arcu. Sed efficitur nisi sit amet lobortis malesuada. Ut quis imperdiet elit. Mauris blandit suscipit leo, non tristique est ultrices eu.</p>
+			<h3>À propos de Pro Ethnographica</h3>
+
+			<h2>Un précieux trésor ethnographique</h2>
+
+<p align="justify">En 1942 le P. Wilhelm Schmidt, premier professeur
+d’ethnologie à l’Université de Fribourg commença à
+rassembler une importante collection ethnographique
+issue de diverses sources. Elle comprend plus de 2800
+pièces de l’Amérique du Nord, de l’Inde, de la
+Papouasie Nouvelle-Guinée, de l’Afrique et de
+l’Australie. Suite à un vol, la collection exposée
+jusque-là dans les vitrines de la Faculté des Lettres
+est restée confinée dans les sous-sols de l’Université.
+Il fallait la sauver.</p>
+<p align="justify">Du temps de la décolonisation, les activités
+scientifiques des missionnaires-ethnologues furent en
+grande partie ignorée, n’étant pas considérées comme
+politiquement correctes. Pourtant leur histoire
+intéresse à nouveau de jeunes chercheurs. Pour faire
+connaître ces collections rares et précieuses, nous
+avons fondé <b> P r o   E t h n o g r a p h i © a </b>  et
+acquis la collection. Ainsi l’avons-nous sauvée !
+Notre but est d’abord de faire sortir la collection de
+l’oubli en établissant un inventaire électronique. En
+outre, nous voulons restaurer et conserver les
+nombreuses pièces qui ont souffert de dommages au long
+des 50 dernières années et les rendre accessibles aux
+intéressés.
+</p>
 		</div>
 		<div class="col-sm-3 col-sm-offset-1">
 			<h6>&nbsp;</h6><address>Archives<br>			100 Second Avenue, 2nd floor<br>			New York, NY 10010</address>
