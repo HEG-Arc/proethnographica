1d238c15e75d6c5601c94de3a511cde0b4b9af98
Adeline Gueniat <gueniat.adeline@gmail.com>
2019-11-21 10:24:55 +0100

    display an image succesfully

--- app/plugins/QrCodeMaker/views/generate_code.php
+++ app/plugins/QrCodeMaker/views/generate_code.php
@@ -11,30 +11,11 @@
 
 <body>
 
+<img src="../../../../qr23.png">	
+ 
 <?php
 
-if(isset($_POST['qr_text']))
-{
-    $fulltext=$_POST['qr_text'];
-    $numbers = explode(",", $fulltext);
-    $folder="qrcodes/";
-    $size=40;
-    echo "<div class='noprint'>";
-    echo count($numbers) . " fiches traitées : " . $fulltext;
-    echo "<button onClick=\"window.print()\">Print this page</button>";
-    echo "</div>";
-    for ($i = 0; $i < count($numbers); $i=$i+1) {
-        $number = $numbers[$i];
-        $file_name="qr". $number .".png";
-        $file_name=$folder.$file_name;
-        QRcode::png($number,$file_name, QR_ECLEVEL_H, 15);
-        echo ". <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br> .<center><img src=\" $file_name \"> . <br> . <span style='font-size: 25px;'><b>Numéro :</b> $number</span></center> . <br> </center>";
-    }
 
 
-
-
-}
 ?>
-
 </body></html>
