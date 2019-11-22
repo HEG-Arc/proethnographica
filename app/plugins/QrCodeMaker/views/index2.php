5dcf9dfa62a2dea71ff537c5c307cc11700c97b9
Adeline Gueniat <gueniat.adeline@gmail.com>
2019-11-22 15:09:00 +0100

    add text to form page

--- app/plugins/QrCodeMaker/views/index2.php
+++ app/plugins/QrCodeMaker/views/index2.php
@@ -2,7 +2,7 @@
 <body>
 <div id="wrapper">
     <form method="post" action="generate">
-        <input type="text" name="qr_text">
+       <h1>Indiquez les numéros d'objets pour lesquels vous souhaitez créer des QR Codes :</h1> <h4>Veillez à séparer les numéros par des virgules.</h4> <input type="text" name="qr_text">
     </form>
 </div>
 </body>
