34acc68fe8a3a584208859d8945fe18782e6754a
Adeline Gueniat <gueniat.adeline@gmail.com>
2019-12-11 18:02:23 +0100

    add button

--- app/plugins/QrCodeMaker/views/index2.php
+++ app/plugins/QrCodeMaker/views/index2.php
@@ -1,8 +1,17 @@
 <html>
 <body>
 <div id="wrapper">
-    <form method="post" action="generate">
-       <h1>Indiquez les numéros d'objets pour lesquels vous souhaitez créer des QR Codes :</h1> <h4>Veillez à séparer les numéros par des virgules.</h4> <input type="text" name="qr_text">
+    <form id="form1" method="post" action="generate">
+	<h1>Indiquez les numéros d'objets pour lesquels vous souhaitez créer des QR Codes :</h1>
+	<h4>Veillez à séparer les numéros par des virgules.</h4>
+	<input type="text" name="qr_text"> 
+	 <a href="javascript:;" onclick="document.getElementById('form1').submit();" class="form-button 1576082343">
+		<span class="form-button">
+			<i class="caIcon fa fa-check-circle-o " style="font-size: 30px;"></i> 
+			Générer
+		</span>
+	</a>
+
     </form>
 </div>
 </body>
