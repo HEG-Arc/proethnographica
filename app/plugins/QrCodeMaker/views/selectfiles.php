a2c63fd9940bbce16c148a0d96168e0da1848969
Adeline Gueniat <gueniat.adeline@gmail.com>
2019-12-12 09:31:35 +0100

    styling

--- app/plugins/QrCodeMaker/views/selectfiles.php
+++ app/plugins/QrCodeMaker/views/selectfiles.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
+
 <html>
 <body>
 
-<form action="upload" method="post" enctype="multipart/form-data">
-    Select image to upload:
-    <input type="file" name="filesToUpload[]" multiple="multiple" id="filesToUpload">
-    <input type="submit" value="Upload Image" name="submit">
+<div id="wrapper">
+    <form id="form2" action="upload" method="post" enctype="multipart/form-data">
+ 	<h1>Sélectionnez les images, avec les QR Codes, que vous souhaitez préparer pour l'import :</h1>
+    	<input type="file" name="filesToUpload[]" multiple="multiple" id="filesToUpload">
+	<a href="javascript:;" onclick="document.getElementById('form2').submit();" class="form-button 1576082343">
+		<span class="form-button">
+			<i class="caIcon fa fa-check-circle-o " style="font-size: 30px;"></i> 
+			Valider
+		</span>
+	</a>
 </form>
+</div>
 
 </body>
 </html>
+
