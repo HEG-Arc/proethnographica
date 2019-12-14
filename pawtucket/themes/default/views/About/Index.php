55f1340ff9691f178ebf8785ca4fad5f9378dd59
Adeline Gueniat <gueniat.adeline@gmail.com>
2019-12-14 13:13:28 +0100

    changed bio text and styling

--- pawtucket/themes/default/views/About/Index.php
+++ pawtucket/themes/default/views/About/Index.php
@@ -4,52 +4,42 @@
 
 	<div class="row">
 		<div class="col-sm-12">
-			<H1><?php print _t("About"); ?></H1>
+			<H1><?php print _t("À propos de Pro Ethnographica"); ?></H1>i
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-sm-5 col-sm-offset-1">
-			<h3>À propos de Pro Ethnographica</h3>
 
-			<h2>Un précieux trésor ethnographique</h2>
+<p>
+<h2>Un précieux trésor ethnographique au cœur de Fribourg !</h2>
+En 1942 le P. Wilhelm Schmidt, premier professeur d’ethnologie à l’Université de Fribourg commença à rassembler une importante collection ethnographique issue de diverses sources. Elle comprend plus de 2800 pièces de l’Amérique du Nord, de l’Inde, de la Papouasie Nouvelle-Guinée, de l’Afrique et de l’Australie. Suite à un vol, la collection exposée jusque-là dans les vitrines de la Faculté des Lettres est restée confinée dans les sous-sols de l’Université. Il fallait la sauver. </p>
 
-<p align="justify">En 1942 le P. Wilhelm Schmidt, premier professeur
-d’ethnologie à l’Université de Fribourg commença à
-rassembler une importante collection ethnographique
-issue de diverses sources. Elle comprend plus de 2800
-pièces de l’Amérique du Nord, de l’Inde, de la
-Papouasie Nouvelle-Guinée, de l’Afrique et de
-l’Australie. Suite à un vol, la collection exposée
-jusque-là dans les vitrines de la Faculté des Lettres
-est restée confinée dans les sous-sols de l’Université.
-Il fallait la sauver.</p>
-<p align="justify">Du temps de la décolonisation, les activités
-scientifiques des missionnaires-ethnologues furent en
-grande partie ignorée, n’étant pas considérées comme
-politiquement correctes. Pourtant leur histoire
-intéresse à nouveau de jeunes chercheurs. Pour faire
-connaître ces collections rares et précieuses, nous
-avons fondé <b> P r o   E t h n o g r a p h i © a </b>  et
-acquis la collection. Ainsi l’avons-nous sauvée !
-Notre but est d’abord de faire sortir la collection de
-l’oubli en établissant un inventaire électronique. En
-outre, nous voulons restaurer et conserver les
-nombreuses pièces qui ont souffert de dommages au long
-des 50 dernières années et les rendre accessibles aux
-intéressés.
+<p>
+<h4>Histoire</h4>
+Du temps de la décolonisation, les activités scientifiques des missionnaires-ethnologues furent en grande partie ignorée, n’étant pas considérées comme politiquement correctes. Pourtant leur histoire intéresse à nouveau de jeunes chercheurs. Pour faire connaître ces collections rares et précieuses, nous avons fondé Pro Ethnographi©a et, avec le soutien de l’Université, acquis la collection. Ainsi l’avons-nous sauvée !
+Notre but est d’abord de faire sortir la collection de l’oubli en établissant un inventaire électronique. En outre, nous voulons restaurer et conserver les nombreuses pièces qui ont souffert de dommages au long des 50 dernières années et les rendre accessibles aux intéressés. 
+
+<h4>La collection</h4>
+La collection s’est constituée entre 1845 et 1980 grâce au travail des Missionnaires et professeurs de la chaire d’ethnologie. La partie la plus ancienne est celle du Père Antoine-Marie Gachet, rare et du plus haut intérêt. Cette collection rassemble des pièces uniques de la tribu des Menominee de l’Amérique de Nord (Wisconsin actuel). Grace aux recherches de Wilhelm Koppers chez les Bhils en Inde centrale entre 1933 et 1939 viennent s’ajouter plus de cent objets – armes, vêtements et accessoires – de cette population. Le Père Georg Höltker a recueilli près de 600 pièces en Papouasie Nouvelle-Guinée dans les années 1936 à 1938 : bijoux, vêtements, armes, symboles cérémoniels, pierres magiques, masques et représentations d’esprits. Ces pièces constituent les rares témoins d’une culture archaïque. Elles émanent de la Vallée de Wagi, du Bismarck district et des régions du Sepik et Ramu. En Afrique, entre 1890 et 1933 MM. Egger, Zumwald, Franconi, Gremoud, Folie, Julmi et Grangier dénichent des objets rares, armes, vêtements, statues, masques et des objets influencés par le christianisme dans les régions du Zaïre, Cameroun, Côte Ivoire et Afrique de Nord.
+
+<h4>Notre but</h4>
+Ce bref résumé veut susciter l’intérêt pour notre travail. Le but est de restituer à la collection toute sa splendeur passée et sa valeur réelle actuelle. Une fois l’inventaire complet effectué et rendu accessible par voie électronique, la prochaine étape sera de faire connaître cette collection dans le monde grâce à des publications. Rejoignez-nous dans cette aventure ethnographique, devenez membre, faites un don ou une donation. Nous vous en remercions vivement !
+
 </p>
 		</div>
 		<div class="col-sm-5 col-sm-offset-1">
-			<h2>Adhérez à l'association</h2>
+			<h2>Nos publications</h2>
 
-<p align="justify">Rejoignez l'association Pro Ethnographica ! Vous pouvez demander (...)</p>
+<p align="justify">Nos publications...</p>
 		</div>
 	</div>
 
 <div class="row">
 		<div class="col-sm-11 col-sm-offset-1">
-			<h2>Publications</h2>
-<p align="justify">Nos publications sont disponibles à l'achat (...)</p>
+			<h2>Adhérez à l'association</h2>
+<p align="justify">Rejoignez vous aussi l'aventure ! Vous pouvez adresser une demande d'adhésion à l'association à Monsieur Hans Werhonig, vice-président.<br />
+<a href="mailto:hans.werhonig@weig.ch?subject=Demande d'adhésion"><span class="glyphicon glyphicon-comment"></span> Envoyer ma demande d'adhésion</a>
+ </p>
 
 </div>
 	</div>
