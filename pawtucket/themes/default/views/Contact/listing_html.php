66460092d529b2e6db3ad5e121b5bc9ad81e66f8
Adeline Gueniat <gueniat.adeline@gmail.com>
2019-12-14 17:01:45 +0100

    added contact info

--- pawtucket/themes/default/views/Contact/listing_html.php
+++ pawtucket/themes/default/views/Contact/listing_html.php
@@ -2,18 +2,17 @@
  <?php
 	$o_config = caGetContactConfig();
 	$va_errors = $this->getVar("errors");
-	MetaTagManager::setWindowTitle($this->request->config->get("app_display_name").": Contact");
-	$vs_page_title = ($o_config->get("contact_page_title")) ? $o_config->get("contact_page_title") : _t("Contact");
+	MetaTagManager::setWindowTitle($this->request->config->get("app_display_name").": Contactez-nous");
 	
 ?>
-<div class="row"><div class="col-sm-12">
-	<H1><?php print $vs_page_title; ?></H1>
+<div class="row">
+	<div class="col-sm-12">
+        	<H1>Contactez-nous</H1>
+			<h5>Des questions, demandes, commentaires ? N'hésitez pas à prendre contact avec nous.</h5>
 
-			<h3>Contactez-nous</h3>
+				<address><h6>Pro Ethnographica</h6> c/o Hans Werhonig <br /> Planche Supérieure 48<br /> CH-1700 Fribourg</address>
 
-				<h6>&nbsp;</h6>	
-				<address>Pro Ethnographica<br>Rue<br>	NPA, Ville</address>
-				<address>Personne de contact<br><span class="info">Téléphone</span> 212 222.2222<br><span class="info">Email</span> <a href="#">info@domain.ch</a></address>
+				<address>Hans Werhonig, vice-président de l'association<br /> <span class="info">Téléphone</span> +41 26 322 14 04 / +41 79 777 86 62 <br><span class="info">Email</span> <a href="mailto:hans.werhonig@weig.ch">hans.werhonig@weig.ch</a></address>
 
 
 
