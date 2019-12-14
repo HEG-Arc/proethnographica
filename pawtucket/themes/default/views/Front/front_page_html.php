e192c8d1e3d277eb8244f7b9c0eadcf65f5ddc98
Adeline Gueniat <gueniat.adeline@gmail.com>
2019-12-14 17:03:40 +0100

    reduced top margin

--- pawtucket/themes/default/views/Front/front_page_html.php
+++ pawtucket/themes/default/views/Front/front_page_html.php
@@ -1,4 +1,4 @@
- 	<div class="row" style="margin-top:2%">
+ 	<div class="row" style="margin-top:1%">
 		<div class="col-sm-10 col-sm-offset-1">
 
 			<H1 style="font-size:50px;font-weight:bold;">Bienvenue sur la vitrine de Pro Ethnographica</H1>
@@ -7,7 +7,7 @@
 <H4>L'association Pro Ethnographica s'est formée dans le but de valoriser et pérénniser ces trésors oubliées. Elle vous présente aujourd'hui cette collection de plusieurs milliers d'objets récoltés par des missionnaires et des explorateurs fribourgeois, dont elle s'occupe avec grand soin. </H4>
 		</div><!--end col-sm-10-->
 	</div><!--end row-->
-	<div class="row" style="margin-top:5%">
+	<div class="row" style="margin-top:2%">
 		<div class="col-sm-12">
 <?php
  print $this->render("Front/featured_set_slideshow_html.php");
